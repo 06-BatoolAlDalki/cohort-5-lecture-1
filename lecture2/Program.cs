f997e6828fa8c7faa418b0810563046245a48864
Batool ALDALKI <gt.batool.aldalki@orange.com>
2025-01-18 20:40:09 +0300

    fff

--- lecture2/Program.cs
+++ lecture2/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace lecture2
 {
+   
     internal class Program
     {
+        static void print()
+        {
+            Console.WriteLine("gggg");
+        }
+
         static void Main(string[] args)
         {
+            print();
+            
             Console.WriteLine("ddddddd");
         }
     }
